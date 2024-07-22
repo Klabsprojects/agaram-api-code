de46420bfdf55ea91fe37fce271c8b7661ba3e55
suthana-klabs <suthana@klabsindia.com>
2024-07-22 14:17:22 +0530

    get private visit posting added

--- src/controllers/forms/privateForeignVisit.controller.js
+++ src/controllers/forms/privateForeignVisit.controller.js
@@ -1,6 +1,7 @@
 const privateVisit = require('../../models/forms/privateForeignVisit.model');
 const { successRes, errorRes } = require("../../middlewares/response.middleware")
 const whatsapp = require('../whatsapp/whatsapp.controller');
+const empProfile = require('../employee/employeeProfile.controller');
 
 // privateVisit creation
 exports.addPrivateForeignVisit = async (req, res) => {
@@ -38,14 +39,64 @@ exports.getPrivateForeignVisit = async (req, res) => {
         try {
             let query = {};
             let data;
+            let resultData = [];
             if(req.query){
                 query.where = req.query;
                 data = await privateVisit.find(req.query).exec();
+                let updateQueryJson = {
+                    empId: data[0].employeeProfileId
+                }
+                uniqueArray = await empProfile.getEmployeeUpdateFilter(updateQueryJson);
+                console.log('length ==> ', uniqueArray.length);
+                if(uniqueArray.length > 0){
+                    console.log('alert')
+                    console.log('data => ', data[0]);
+                    let dataAll = {
+                        toPostingInCategoryCode: uniqueArray[0].transferOrPostingEmployeesList[0].toPostingInCategoryCode,
+                        toDepartmentId: uniqueArray[0].transferOrPostingEmployeesList[0].toDepartmentId,
+                        toDesignationId: uniqueArray[0].transferOrPostingEmployeesList[0].toDesignationId,
+                        postTypeCategoryCode: uniqueArray[0].transferOrPostingEmployeesList[0].postTypeCategoryCode,
+                        locationChangeCategoryId: uniqueArray[0].transferOrPostingEmployeesList[0].locationChangeCategoryId,
+                        remarks: uniqueArray[0].remarks,
+                        updateType: uniqueArray[0].updateType,
+                        orderTypeCategoryCode: uniqueArray[0].orderTypeCategoryCode,
+                        orderNumber: uniqueArray[0].orderNumber,
+                        orderForCategoryCode: uniqueArray[0].orderForCategoryCode,
+                        dateOfOrder: uniqueArray[0].dateOfOrder,
+
+
+
+                        approvalStatus: data[0].approvalStatus,
+                        _id: data[0]._id,
+                        officer_name: data[0].officer_name,
+                        employeeProfileId: data[0].employeeProfileId,
+                        designation: data[0].designation,
+                        designationId: data[0].designationId,
+                        department: data[0].department,
+                        departmentId: data[0].departmentId,
+                        selfOrFamily: data[0].selfOrFamily,
+                        dateOfOrder: data[0].dateOfOrder,
+                        orderType: data[0].orderType,
+                        orderNo: data[0].orderNo,
+                        orderFor: data[0].orderFor,
+                        remarks: data[0].remarks,
+                        orderFile: data[0].orderFile,
+                        proposedCountry: data[0].proposedCountry,
+                        fromDate: data[0].fromDate,
+                        toDate: data[0].toDate,
+                        fundSource: data[0].fundSource,
+                        status: data[0].status,
+                    }
+            resultData.push(dataAll);
+                }
+        successRes(res, resultData, 'privateVisit listed Successfully');
             }
-            else
+            else{
                 data = await privateVisit.find();
             //res.json(data);
             successRes(res, data, 'privateVisit listed Successfully');
+            }
+                
         } catch (error) {
             console.log('error', error);
             //res.json(error);
