db7de552f07b2131f56b414b45a4790c207c99ae
suthana-klabs <suthana@klabsindia.com>
2024-08-07 17:15:39 +0530

    leave, training api bug resolved for updateType changes

--- src/controllers/employee/leave.controller.js
+++ src/controllers/employee/leave.controller.js
@@ -59,32 +59,45 @@ exports.getLeave = async (req, res) => {
                 req.query.loginAs == 'Spl A - ASO' || 
                 req.query.loginAs == 'Spl B - ASO'
             ){
-                // Step 1: Find the user IDs where loginAs is 'adminLogin'
                 if(req.query.loginAs == 'Spl A - SO'){
-                    admins  = await login.find({ loginAs: { $in: ['Spl A - SO', 'Spl A - ASO'] } }).select('_id').exec();
+                    admins  = await login.find({ loginAs: { $in: ['Spl A - ASO', 'Spl A - SO'] } }).select('_id').exec();
                     if (admins .length === 0) {
                         return res.status(404).json({ message: 'No admin users found' });
                     }   
-                }
-                else if(req.query.loginAs == 'Spl B - SO'){
-                    admins  = await login.find({ loginAs: { $in: ['Spl B - SO', 'Spl B - ASO'] } }).select('_id').exec();
-                    if (admins .length === 0) {
-                        return res.status(404).json({ message: 'No admin users found' });
-                    }   
-                }
-                else if(req.query.loginAs == 'Spl A - ASO' || req.query.loginAs == 'Spl B - ASO'){
-                    adminIds.push(req.query.loginId);
-                }
-                
-                 if(req.query.loginAs == 'Spl A - SO' ||
-                    req.query.loginAs == 'Spl B - SO')
-                {
-                    adminIds = admins.map(admin => admin._id);
-                }
-                console.log('admins ', admins);
-                console.log('adminIds ', adminIds);
+            }
+            if(req.query.loginAs == 'Spl A - ASO'){
+                admins  = await login.find({ loginAs: { $in: ['Spl A - ASO'] } }).select('_id').exec();
+                if (admins .length === 0) {
+                    return res.status(404).json({ message: 'No admin users found' });
+                }   
+            }
+            if(req.query.loginAs == 'Spl B - SO'){
+                admins  = await login.find({ loginAs: { $in: ['Spl B - ASO', 'Spl B - SO'] } }).select('_id').exec();
+                if (admins .length === 0) {
+                    return res.status(404).json({ message: 'No admin users found' });
+                }   
+            }
+            if(req.query.loginAs == 'Spl B - ASO'){
+                admins  = await login.find({ loginAs: { $in: ['Spl B - ASO'] } }).select('_id').exec();
+                if (admins .length === 0) {
+                    return res.status(404).json({ message: 'No admin users found' });
+                }   
+            }
+            if(req.query.loginAs == 'Spl A - SO' || req.query.loginAs == 'Spl A - ASO' ||
+                req.query.loginAs == 'Spl B - SO' || req.query.loginAs == 'Spl B - ASO')
+            {
+                adminIds = admins.map(admin => admin._id);
+            }
+            console.log('admins ', admins);
+            console.log('adminIds ', adminIds);
+            let profileQuery = {
+                $or: [
+                    { submittedBy: { $in: adminIds } },
+                    { approvalStatus: true }
+                ]
+            }
                  // Step 2: Query the leave collection where submittedBy matches any of the admin IDs
-                 data = await leave.find({ submittedBy: { $in: adminIds } })
+                 data = await leave.find(profileQuery)
                      .populate({
                          path: 'employeeProfileId',
                          model: 'employeeProfile',
@@ -108,14 +121,6 @@ exports.getLeave = async (req, res) => {
                     console.log(data, 'leave listed else Successfully');
                     successRes(res, data, 'leave listed Successfully');
                 }
-
-                // data = await leave.find()
-                // .populate({
-                //     path: 'employeeProfileId',
-                //     model: 'employeeProfile', // Model of the application collection
-                //     select: ['batch', 'mobileNo1'] // Fields to select from the application collection
-                // })  
-                // .exec();
         } catch (error) {
             console.log('error', error);
             errorRes(res, error, "Error on listing leave");
