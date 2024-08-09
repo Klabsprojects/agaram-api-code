c52928e927804b660c490fdf329ec454c16dd666
suthana-klabs <suthana@klabsindia.com>
2024-08-09 15:50:39 +0530

    submittedBy, approvedBy changes for intimation form

--- src/controllers/forms/intimation.controller.js
+++ src/controllers/forms/intimation.controller.js
@@ -33,7 +33,7 @@ exports.addIntimation = async (req, res) => {
     }
 
 // Get intimation
-exports.getIntimation = async (req, res) => {
+exports.getIntimationOld = async (req, res) => {
         console.log('helo from intimation controller', req.query);
         try {
             let query = {};
@@ -203,6 +203,300 @@ exports.getIntimation = async (req, res) => {
             errorRes(res, error, "Error on listing intimation");
         }
     }
+
+    exports.getIntimation = async (req, res) => {
+        console.log('helo from education controller', req.query);
+        try {
+            let query = {};
+            let data;
+            let resultData = [];
+            let admins = [];
+            let adminIds = [];
+            if(req.query._id){
+                query.where = req.query;
+                data = await intimation.find(req.query)
+                .populate({
+                    path: 'employeeProfileId',
+                    model: 'employeeProfile', // Model of the application collection
+                    select: ['batch', 'mobileNo1'] // Fields to select from the application collection
+                })  
+                .populate({
+                    path: 'submittedBy',
+                    model: 'login', // Ensure the model name matches exactly
+                    select: ['username', 'loginAs'] // Specify the fields you want to include from EmployeeProfile
+                })
+                .populate({
+                    path: 'approvedBy',
+                    model: 'login', // Ensure the model name matches exactly
+                    select: ['username', 'loginAs'] // Specify the fields you want to include from EmployeeProfile
+                }) 
+                .exec();
+                if(data.length > 0){
+                    console.log('data.length', data.length)
+                    for(let data0 of data){
+                        console.log('IDDD => ', data0);
+                        console.log('IDDD => ', data0.employeeProfileId._id);
+                        let updateQueryJson = {
+                            empId: data0.employeeProfileId._id
+                        }
+                uniqueArray = await empProfile.getEmployeeUpdateFilter(updateQueryJson);
+                console.log('length ==> ', uniqueArray.length);
+                        if(uniqueArray.length > 0 && uniqueArray[0].transferOrPostingEmployeesList){
+                            for(let transferOrPostingEmployeesList of uniqueArray[0].transferOrPostingEmployeesList){
+                                console.log('Check ', transferOrPostingEmployeesList.fullName, transferOrPostingEmployeesList.empProfileId._id.toString(),
+                                data0.employeeProfileId._id.toString());
+                                if(transferOrPostingEmployeesList.empProfileId._id.toString() === data0.employeeProfileId._id.toString()){
+                                    console.log('Matched ');
+                                    console.log('posting available')
+                            dataAll = {
+                                toPostingInCategoryCode: transferOrPostingEmployeesList.toPostingInCategoryCode,
+                                toDepartmentId: transferOrPostingEmployeesList.toDepartmentId,
+                                toDesignationId: transferOrPostingEmployeesList.toDesignationId,
+                                postTypeCategoryCode: transferOrPostingEmployeesList.postTypeCategoryCode,
+                                locationChangeCategoryId: transferOrPostingEmployeesList.locationChangeCategoryId,
+                                updateType: uniqueArray[0].updateType,
+                                orderTypeCategoryCode: uniqueArray[0].orderTypeCategoryCode,
+                                orderNumber: uniqueArray[0].orderNumber,
+                                orderForCategoryCode: uniqueArray[0].orderForCategoryCode,
+                        
+                                _id: data0._id,
+                                officerName: data0.officerName,
+                                employeeProfileId: data0.employeeProfileId,
+                                designation: data0.designation,
+                                designationId: data0.designationId,
+                                department: data0.department,
+                                departmentId: data0.departmentId,
+                                selfOrFamily: data0.selfOrFamily,
+                                detailsOfIntimation: data0.detailsOfIntimation,
+                                fundSource: data0.fundSource,
+                                typeOfIntimation: data0.typeOfIntimation,
+                                previousSanctionOrder: data0.previousSanctionOrder,
+                                dateOfOrder: data0.dateOfOrder,
+                                orderType: data0.orderType,
+                                orderNo: data0.orderNo,
+                                orderFor: data0.orderFor,
+                                remarks: data0.remarks,
+                                orderFile: data0.orderFile,
+                                submittedBy: data0.submittedBy,
+                                approvedBy: data0.approvedBy,
+                                approvedDate: data0.approvedDate,
+                                approvalStatus: data0.approvalStatus,
+                    }
+            resultData.push(dataAll);
+                }
+                    }
+                }
+                else{
+                    let dataAll = {
+                        _id: data0._id,
+                        officerName: data0.officerName,
+                        employeeProfileId: data0.employeeProfileId,
+                        designation: data0.designation,
+                        designationId: data0.designationId,
+                        department: data0.department,
+                        departmentId: data0.departmentId,
+                        selfOrFamily: data0.selfOrFamily,
+                        detailsOfIntimation: data0.detailsOfIntimation,
+                        fundSource: data0.fundSource,
+                        typeOfIntimation: data0.typeOfIntimation,
+                        previousSanctionOrder: data0.previousSanctionOrder,
+                        dateOfOrder: data0.dateOfOrder,
+                        orderType: data0.orderType,
+                        orderNo: data0.orderNo,
+                        orderFor: data0.orderFor,
+                        remarks: data0.remarks,
+                        orderFile: data0.orderFile,
+                        submittedBy: data0.submittedBy,
+                        approvedBy: data0.approvedBy,
+                        approvedDate: data0.approvedDate,
+                        approvalStatus: data0.approvalStatus,
+                    }
+            resultData.push(dataAll);
+                }
+            }
+                }
+                else
+                {
+                    resultData = [];
+                }
+                successRes(res, resultData, 'education listed Successfully');
+            }
+            else if(req.query.loginAs == 'Spl A - SO' ||
+                req.query.loginAs == 'Spl B - SO' ||
+                req.query.loginAs == 'Spl A - ASO' || 
+                req.query.loginAs == 'Spl B - ASO'
+            ){
+            if(req.query.loginAs == 'Spl A - SO'){
+                    admins  = await login.find({ loginAs: { $in: ['Spl A - ASO', 'Spl A - SO'] } }).select('_id').exec();
+                    if (admins .length === 0) {
+                        return res.status(404).json({ message: 'No admin users found' });
+                    }   
+            }
+            if(req.query.loginAs == 'Spl A - ASO'){
+                admins  = await login.find({ loginAs: { $in: ['Spl A - ASO'] } }).select('_id').exec();
+                if (admins .length === 0) {
+                    return res.status(404).json({ message: 'No admin users found' });
+                }   
+            }
+            if(req.query.loginAs == 'Spl B - SO'){
+                admins  = await login.find({ loginAs: { $in: ['Spl B - ASO', 'Spl B - SO'] } }).select('_id').exec();
+                if (admins .length === 0) {
+                    return res.status(404).json({ message: 'No admin users found' });
+                }   
+            }
+            if(req.query.loginAs == 'Spl B - ASO'){
+                admins  = await login.find({ loginAs: { $in: ['Spl B - ASO'] } }).select('_id').exec();
+                if (admins .length === 0) {
+                    return res.status(404).json({ message: 'No admin users found' });
+                }   
+            }
+            if(req.query.loginAs == 'Spl A - SO' || req.query.loginAs == 'Spl A - ASO' ||
+                req.query.loginAs == 'Spl B - SO' || req.query.loginAs == 'Spl B - ASO')
+            {
+                adminIds = admins.map(admin => admin._id);
+            }
+            console.log('admins ', admins);
+            console.log('adminIds ', adminIds);
+            let profileQuery = {
+                $or: [
+                    { submittedBy: { $in: adminIds } },
+                    { approvalStatus: true }
+                ]
+            }
+                 // Step 2: Query the leave collection where submittedBy matches any of the admin IDs
+                 data = await intimation.find(profileQuery)
+                     .populate({
+                         path: 'employeeProfileId',
+                         model: 'employeeProfile',
+                         select: ['batch', 'mobileNo1']
+                     })
+                     .populate({
+                        path: 'submittedBy',
+                        model: 'login', // Ensure the model name matches exactly
+                        select: ['username', 'loginAs'] // Specify the fields you want to include from EmployeeProfile
+                    })
+                    .populate({
+                        path: 'approvedBy',
+                        model: 'login', // Ensure the model name matches exactly
+                        select: ['username', 'loginAs'] // Specify the fields you want to include from EmployeeProfile
+                    }) 
+                     .exec();   
+                     if(data.length > 0){
+                        console.log('data.length', data.length)
+                    for(let data0 of data){
+                        console.log('IDDD => ', data0);
+                        console.log('IDDD => ', data0.employeeProfileId._id);
+                        let updateQueryJson = {
+                            empId: data0.employeeProfileId._id
+                        }
+                        uniqueArray = await empProfile.getEmployeeUpdateFilter(updateQueryJson);
+                        console.log('length ==> ', uniqueArray.length);
+                        if(uniqueArray.length > 0 && uniqueArray[0].transferOrPostingEmployeesList){
+                            for(let transferOrPostingEmployeesList of uniqueArray[0].transferOrPostingEmployeesList){
+                                console.log('Check ', transferOrPostingEmployeesList.fullName, transferOrPostingEmployeesList.empProfileId._id.toString(),
+                                data0.employeeProfileId._id.toString());
+                                if(transferOrPostingEmployeesList.empProfileId._id.toString() === data0.employeeProfileId._id.toString()){
+                                    console.log('Matched ');
+                                    console.log('posting available')
+                            dataAll = {
+                                toPostingInCategoryCode: transferOrPostingEmployeesList.toPostingInCategoryCode,
+                                toDepartmentId: transferOrPostingEmployeesList.toDepartmentId,
+                                toDesignationId: transferOrPostingEmployeesList.toDesignationId,
+                                postTypeCategoryCode: transferOrPostingEmployeesList.postTypeCategoryCode,
+                                locationChangeCategoryId: transferOrPostingEmployeesList.locationChangeCategoryId,
+                                updateType: uniqueArray[0].updateType,
+                                orderTypeCategoryCode: uniqueArray[0].orderTypeCategoryCode,
+                                orderNumber: uniqueArray[0].orderNumber,
+                                orderForCategoryCode: uniqueArray[0].orderForCategoryCode,
+                                _id: data0._id,
+                                officerName: data0.officerName,
+                                employeeProfileId: data0.employeeProfileId,
+                                designation: data0.designation,
+                                designationId: data0.designationId,
+                                department: data0.department,
+                                departmentId: data0.departmentId,
+                                selfOrFamily: data0.selfOrFamily,
+                                detailsOfIntimation: data0.detailsOfIntimation,
+                                fundSource: data0.fundSource,
+                                typeOfIntimation: data0.typeOfIntimation,
+                                previousSanctionOrder: data0.previousSanctionOrder,
+                                dateOfOrder: data0.dateOfOrder,
+                                orderType: data0.orderType,
+                                orderNo: data0.orderNo,
+                                orderFor: data0.orderFor,
+                                remarks: data0.remarks,
+                                orderFile: data0.orderFile,
+                                submittedBy: data0.submittedBy,
+                                approvedBy: data0.approvedBy,
+                                approvedDate: data0.approvedDate,
+                                approvalStatus: data0.approvalStatus,
+                            }
+                    resultData.push(dataAll);
+                        }
+                        
+                    }
+                        }
+                        else{
+                            dataAll = {
+                                _id: data0._id,
+                                officerName: data0.officerName,
+                                employeeProfileId: data0.employeeProfileId,
+                                designation: data0.designation,
+                                designationId: data0.designationId,
+                                department: data0.department,
+                                departmentId: data0.departmentId,
+                                selfOrFamily: data0.selfOrFamily,
+                                detailsOfIntimation: data0.detailsOfIntimation,
+                                fundSource: data0.fundSource,
+                                typeOfIntimation: data0.typeOfIntimation,
+                                previousSanctionOrder: data0.previousSanctionOrder,
+                                dateOfOrder: data0.dateOfOrder,
+                                orderType: data0.orderType,
+                                orderNo: data0.orderNo,
+                                orderFor: data0.orderFor,
+                                remarks: data0.remarks,
+                                orderFile: data0.orderFile,
+                                submittedBy: data0.submittedBy,
+                                approvedBy: data0.approvedBy,
+                                approvedDate: data0.approvedDate,
+                                approvalStatus: data0.approvalStatus,
+                            }
+                    resultData.push(dataAll);
+                        }
+                    }
+                    }
+                    else
+                    {
+                        resultData = [];
+                    }
+            successRes(res, resultData, 'education listed Successfully');
+            }
+            else{
+                data = await intimation.find()
+                .populate({
+                    path: 'employeeProfileId',
+                    model: 'employeeProfile', // Model of the application collection
+                    select: ['batch', 'mobileNo1'] // Fields to select from the application collection
+                })  
+                .populate({
+                    path: 'submittedBy',
+                    model: 'login', // Ensure the model name matches exactly
+                    select: ['username', 'loginAs'] // Specify the fields you want to include from EmployeeProfile
+                })
+                .populate({
+                    path: 'approvedBy',
+                    model: 'login', // Ensure the model name matches exactly
+                    select: ['username', 'loginAs'] // Specify the fields you want to include from EmployeeProfile
+                }) 
+                .exec();
+            successRes(res, data, 'education listed Successfully');
+            }
+                
+        } catch (error) {
+            console.log('error', error);
+            errorRes(res, error, "Error on listing education");
+        }
+    }    
 
     exports.updateIntimation = async (req, res) => {
         try {
