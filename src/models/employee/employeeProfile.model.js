26471801c6a79fa9879bd6032dd93fda5c24e787
suthana-klabs <suthana@klabsindia.com>
2024-08-17 19:21:31 +0530

    post, put employee profile api changes completed

--- src/models/employee/employeeProfile.model.js
+++ src/models/employee/employeeProfile.model.js
@@ -23,9 +23,26 @@ const employeeProfileSchema = new Schema({
 		courseLevel: String,
 		specialisation: String,
 		degree: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'degree' // This references the degree model
+			type: Schema.Types.Mixed, // Allows any type
+			validate: {
+				validator: function(value) {
+					// Check if the value is an empty string or a valid ObjectId
+					return value === '' || mongoose.Types.ObjectId.isValid(value);
+				},
+				message: 'Invalid degree ID or empty string.'
+			},
+			default: ''
 		},
+		// degree1: {
+		// 	type: mongoose.Schema.Types.ObjectId,
+		// 	ref: 'degree', 
+		// 	validate: {
+		// 		validator: function(value) {
+		// 			return value === undefined || value === null || value === '' || mongoose.Types.ObjectId.isValid(value);
+		// 		},
+		// 		message: 'Invalid degree ID or empty string.'
+		// 	}
+		// },
 		instituteName: String,
 		locationState: String,
 		locationCountry: String,
