fbd560e11f54cc00a3abfa881c4c5525001ec7d9
suthana-klabs <suthana@klabsindia.com>
2025-02-11 10:46:22 +0530

    dro route

---
+++ src/routes/employee/droProfile.routes.js
@@ -0,0 +1,25 @@
+module.exports = (app) => {
+    const value = require("../../controllers/employee/droProfile.controller");
+    const {  jwt, ERRORS, SUCCESS, Op } = require("../../helpers/index.helper");
+    const upload = require("../../middlewares/upload")
+    //const uploadImage = require("../../middlewares/uploadImage");
+    const CommonFileUpload = require("../../middlewares/commonfileupload");
+   app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.post(
+    "/addDroProfile",
+    [jwt.verifyToken],
+    CommonFileUpload("droProfile").fields([
+        { name: "imagePath", maxCount: 1 },
+        { name: "orderFile", maxCount: 1 },
+    ]),
+    value.addDroProfile
+  );
+
+}
