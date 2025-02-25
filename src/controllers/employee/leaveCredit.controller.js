dda1f6b9ea0aa69229f0c925c8dae44501a420ba
suthana-klabs <suthana@klabsindia.com>
2025-02-25 17:52:59 +0530

    update leave credit completed

--- src/controllers/employee/leaveCredit.controller.js
+++ src/controllers/employee/leaveCredit.controller.js
@@ -19,34 +19,43 @@ const employeeProfile = require('../../models/employee/employeeProfile.model');
                     filter = {
                         _id : query.id
                     }
+                    console.log('update ', update);
+                    console.log('filter ', filter);
+                    // Check if the update object is empty or not
+                    if (Object.keys(update).length > 0) {
+                        console.log('value got');
+                        const data = await leaveCredit.findOneAndUpdate(filter, update, {
+                            new: true
+                        });
+                        console.log('data updated ', data);
+                        successRes(res, data, 'data updated Successfully');
+                    } else {
+                        console.log('empty');
+                        throw 'Update value missing';
+                    }
                 }
-                
-            else{
-                console.log('id not coming');
-                throw 'pls provide id field';
-            }
+                else{
+                    console.log('id not coming');
+                    //throw 'pls provide id field';
+                    let leaveCreditData = {
+                        casualLeave: req.body.casualLeave,
+                        restrictedHoliday: req.body.restrictedHoliday,
+                        earnedLeave: req.body.earnedLeave,
+                        halfPayLeave: req.body.halfPayLeave,
+                        empProfileId: req.body.empProfileId
+                        }
+                    console.log('leaveCreditData ', leaveCreditData)
+                    const leaveCreditDataRes = await leaveCredit.create(leaveCreditData);
+                    successRes(res, leaveCreditDataRes, 'data created Successfully');
+                }
+
             }
             
             else{
                 console.log('problem in input query');
-                throw 'pls provideinput query';
-            }
+                throw 'pls provide input query';
+            }      
             
-                
-            console.log('update ', update);
-            console.log('filter ', filter);
-            // Check if the update object is empty or not
-            if (Object.keys(update).length > 0) {
-                console.log('value got');
-                const data = await leaveCredit.findOneAndUpdate(filter, update, {
-                    new: true
-                  });
-                console.log('data updated ', data);
-                successRes(res, data, 'data updated Successfully');
-            } else {
-                console.log('empty');
-                throw 'Update value missing';
-            }
         } catch (error) {
             console.log('catch update', error);
             errorRes(res, error, "Error on updation");
